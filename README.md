975ca669530d3b343dd0dbce25024223af575297
Bikaze <clmntmugisha@gmail.com>
2023-10-15 20:45:19 +0200

    something

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is a mini-printf project (#ALX)
+Hello
--- main.h
+++ main.h
@@ -1,8 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
-int _putchar(char);
-int print_str(char *);
-int print_format(char, va_list);
+
 int _printf(const char *format, ...);
+
 #endif
+
